c58675dc41cb8d0432e1d82a2aa9eb39403ea293
Ejike Odoh <ejike.developer@gmail.com>
2026-05-21 10:14:12 +0100

    Start implementation of functionalities

---
+++ js/index.js
@@ -0,0 +1,13 @@
+
+    let records = [
+        { id: 1, name: "Amina Bello", currentClass: "SS1", gender: "Female", age: 14 },
+        { id: 2, name: "Chinedu Okafor", currentClass: "SS2", gender: "Male", age: 15 },
+        { id: 3, name: "Fatima Musa", currentClass: "SS1", gender: "Female", age: 13 },
+        { id: 4, name: "Ibrahim Sadiq", currentClass: "SS3", gender: "Male", age: 16 },
+        { id: 5, name: "Blessing Nwoye", currentClass: "SS2", gender: "Female", age: 15 },
+        { id: 6, name: "Tunde Adeyemi", currentClass: "SS1", gender: "Male", age: 14 },
+        { id: 7, name: "Zainab Abdullahi", currentClass: "SS3", gender: "Female", age: 16 },
+        { id: 8, name: "Emeka Obi", currentClass: "SS2", gender: "Male", age: 15 },
+        { id: 9, name: "Hauwa Usman", currentClass: "SS1", gender: "Female", age: 13 },
+        { id: 10, name: "Sola Ogunleye", currentClass: "SS3", gender: "Male", age: 17 }
+    ]
